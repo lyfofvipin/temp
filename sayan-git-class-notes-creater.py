7f67a8b998867ba1ede7b1fb390dcffa656a5072
kumar vipin yadav <kumarvipinyadav369@gmail.com>
2018-07-06 14:55:41 +0530

    Update sayan-git-class-notes-creater.py

--- sayan-git-class-notes-creater.py
+++ sayan-git-class-notes-creater.py
@@ -1,6 +1,6 @@
 import requests
 req = requests.get('https://dgplug.org/irclogs/2018/Logs-2018-07-04-13-29.txt')
-fobj = open('sayanclass.txt','w')
+with open('sayanclass.txt','w') as fobj
 l = [ x[7:] for x in str(req.content).split('\\n') if '*-' in x ]
 for x in l:
     fobj.write(x+'\n')
