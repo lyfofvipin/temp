65fc2947e37b6a235b3c7fb0d3e5889542c86a89
kumar vipin yadav <kumarvipinyadav369@gmail.com>
2020-04-24 13:56:46 +0530

    to make a swuare of given number

---
+++ square.py
@@ -0,0 +1,10 @@
+n = int(input("Enter the number you wish"))
+i = 1
+while(i <= n):
+    if (i == 1 or i == n):
+        print("*" * n)
+    else:
+        a = 9
+        b = 90
+        print(a,b)
+    i += 1
