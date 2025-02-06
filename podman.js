f571aa4d35df1ee9ea232fe7fb482eb7219e9b19
kumar vipin yadav <kumarvipinyadav369@gmail.com>
2025-02-06 23:35:00 +0530

    Create podman.js with default pm module
    Create podman.js with default pm module

---
+++ podman.js
@@ -0,0 +1,29 @@
+pm.test("Status code is 200", function () {
+    pm.response.to.have.status(200);
+});
+
+pm.test("Body matches string", function () {
+    pm.expect(pm.response.text()).to.include("kernel-2.6.9-5");
+});
+
+pm.test("Your test name", function () {
+    var jsonData = pm.response.json();
+    pm.expect(jsonData.RHEL["4.0"].ga[0]).to.eql("kernel-2.6.9-5");
+});
+
+pm.test("Status code name has string", function () {
+    pm.response.to.have.status("OK");
+});
+
+var schema = {
+    "type": "object",
+    "RHEL": {
+        "type": "object"
+    }
+};
+
+pm.test('Schema is valid', function () {
+    
+    var jsonData = pm.response.json();
+    pm.expect(tv4.validate(jsonData, schema)).to.be.true;
+});
