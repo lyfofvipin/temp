1a941ad1b7f2cf796970a9d3f1b9018c7db04aec
kumar vipin yadav <kumarvipinyadav369@gmail.com>
2025-05-07 20:01:38 +0530

    Update pythondemo.py

--- pythondemo.py
+++ pythondemo.py
@@ -234,3 +234,17 @@
     
 #     case "stream":
 #         print(stream)
+
+# import sqlite3
+# conn = sqlite3.connect("database.db")
+# cursor = conn.cursor()
+
+# cursor.execute("CREATE TABLE movie(title, year, score)")
+
+# cursor.execute("""
+#     INSERT INTO movie VALUES
+#         ('Monty Python and the Holy Grail', 1975, 8.2),
+#         ('And Now for Something Completely Different', 1971, 7.5)
+# """)
+
+# conn.commit()
