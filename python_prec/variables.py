37a1b0fb9424964a53c32adb3ebf28c402f8b0e3
kumar vipin yadav <kumarvipinyadav369@gmail.com>
2025-10-16 11:08:56 +0530

    Update variables.py

--- python_prec/variables.py
+++ python_prec/variables.py
@@ -17,34 +17,12 @@ print( number )
 print( type(number) )
 
 
+# The name of a variable can be composed of letters, digits and the underscores.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#     A variable names can not have any character then underscore _.
+#     A variable names can not start with an numeric value
+#     Variable names in C are case sensitive ( variable name system and System are not same ).
+#     It should not be a keyword of python.
 
 
 
